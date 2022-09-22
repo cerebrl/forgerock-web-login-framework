76ff1935656328d1aa6c198a0ba08e7c77fc0d23
ryanbas21 <ryan.basmajian@forgerock.com>
2022-09-22 14:44:05 -0600

    chore: pr-review
    inital pr comments that I could resolve without additional questions

--- src/lib/journey/callbacks/text-output/text-output.svelte
+++ src/lib/journey/callbacks/text-output/text-output.svelte
@@ -1,31 +1,48 @@
 <script lang="ts">
   import type { ConfirmationCallback, TextOutputCallback } from '@forgerock/javascript-sdk';
-  import Centered from '$components/primitives/box/centered.svelte';
+
   import Button from '$components/primitives/button/button.svelte';
 
+  import xss from 'xss';
   export let callback: TextOutputCallback;
   export let choice: ConfirmationCallback;
 
   const options = choice.getOptions();
-  $: selected = choice.getDefaultOption(); // gets the index position
 
-  const message = callback.getMessage();
+  $: message = callback.getMessage();
+
+  const ignoreBody = ['script'];
+  const sanitized = xss(message, {
+    whiteList: {},
+    stripIgnoreTag: true,
+    stripIgnoreTagBody: ignoreBody,
+    onTag: function onTag(tag, _html, options) {
+      // callback which will filter out all script tags and replace it with an empty string
+      // Do not process the contents of stripIgnoreTagBody and do nothing
+      // got this from https://github.com/leizongmin/js-xss/issues/242
+      if (!options.isWhite && options.isClosing && ignoreBody.indexOf(tag) === -1) {
+        return '';
+      }
+    },
+  });
+
+  $: selected = choice.getDefaultOption(); // gets the index position
 
   function setValue(index: number) {
     selected = index;
   }
 </script>
 
-<Centered>
-  <div>{message}</div>
-  {#each options as opt, index}
-    <Button
-      style={selected === index ? 'primary' : 'secondary'}
-      type="button"
-      width="auto"
-      onClick={() => setValue(index)}
-    >
-      {opt}
-    </Button>
-  {/each}
-</Centered>
+<p class="tw_text-base tw_text-center tw_py-4 tw_text-secondary-dark dark:tw_text-secondary-light">
+  {sanitized}
+</p>
+{#each options as opt, index}
+  <Button
+    style={selected === index ? 'primary' : 'secondary'}
+    type="button"
+    width="auto"
+    onClick={() => setValue(index)}
+  >
+    {opt}
+  </Button>
+{/each}

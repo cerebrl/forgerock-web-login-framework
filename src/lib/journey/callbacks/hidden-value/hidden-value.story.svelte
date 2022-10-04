594b1d96f7302451fa0e240bf9b5e33c334c00b9
David Adams <david.adams@forgerock.com>
2022-10-04 13:25:43 +0100

    feat(callbacks): add hidden-value callback

---
+++ src/lib/journey/callbacks/hidden-value/hidden-value.story.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import type { ChoiceCallback } from '@forgerock/javascript-sdk';
+
+  import Centered from '$components/primitives/box/centered.svelte';
+  import HiddenValue from './hidden-value.svelte';
+
+  export let callback: ChoiceCallback;
+</script>
+
+<Centered>
+  <HiddenValue {callback} idx={0} />
+</Centered>
---
+++ src/lib/journey/callbacks/hidden-value/hidden-value.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import type { HiddenValueCallback } from '@forgerock/javascript-sdk';
+
+  export let callback: HiddenValueCallback;
+  export let idx: number;
+
+  const inputName = callback?.payload?.input?.[0].name || `hidden-value-${idx}`;
+  const inputValue = callback?.getInputValue();
+</script>
+
+<input type="hidden" name={inputName} id={inputName} value={inputValue} />

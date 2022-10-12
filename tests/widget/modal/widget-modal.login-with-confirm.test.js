c3c7d8121429678686cd0a4575bd0d8cea0156b7
David Adams <david.adams@forgerock.com>
2022-10-12 14:10:03 +0100

    feat(callbacks): update ordering of confirmation and choice callbacks e2e test

--- tests/widget/modal/widget-modal.login-with-confirm.test.js
+++ tests/widget/modal/widget-modal.login-with-confirm.test.js
@@ -1,7 +1,7 @@
 import { expect, test } from '@playwright/test';
 
 test('Modal widget with simple login and confirmation', async ({ page }) => {
-  await page.goto('widget/modal?journey=LoginWithConfirmationAndChoice');
+  await page.goto('widget/modal?journey=LoginTestCallbacks');
 
   const loginButton = page.locator('button', { hasText: 'Open Login Modal' });
   await loginButton.click();
@@ -9,12 +9,12 @@ test('Modal widget with simple login and confirmation', async ({ page }) => {
   await page.fill('text="User Name"', 'demouser');
   await page.locator('button', { hasText: 'Next' }).click();
 
-  await page.fill('text=Password', 'j56eKtae*1');
-  await page.locator('button', { hasText: 'Next' }).click();
-
   // Confirmation
   expect(await page.locator('text="Are you human?"')).toContainText('Are you human?');
   await page.selectOption('select', '0');
+  await page.locator('button', { hasText: 'Next' }).click();
+
+  await page.fill('text=Password', 'j56eKtae*1');
   await page.locator('button', { hasText: 'Next' }).click();
 
   // Choice

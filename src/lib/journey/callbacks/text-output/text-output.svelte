c543de344ca035d7d92643555312436715d110d3
ryanbas21 <ryan.basmajian@forgerock.com>
2023-01-18 14:23:21 -0700

    refactor(move button to text-output component): remove knowledge of callback from generic
    generic.svelte shouldn't need to know information about hte callback specifics, so we are going to
    move the logic from the main svelte render, to the component specific area. this allows text-output
    callback component to remain knowledgeable about what is required without leaking that information
    out into the ether of other components

--- src/lib/journey/callbacks/text-output/text-output.svelte
+++ src/lib/journey/callbacks/text-output/text-output.svelte
@@ -3,6 +3,8 @@
   import sanitize from 'xss';
 
   import Text from '$components/primitives/text/text.svelte';
+  import Button  from '$components/primitives/button/button.svelte';
+  import T from '$components/_utilities/locale-strings.svelte';
 
   import type {
     CallbackMetadata,
@@ -15,23 +17,31 @@
   // Unused props. Setting to const prevents errors in console
   export const callbackMetadata: Maybe<CallbackMetadata> = null;
   export const selfSubmitFunction: Maybe<SelfSubmitFunction> = null;
-  export const stepMetadata: Maybe<StepMetadata> = null;
+  export let stepMetadata: Maybe<StepMetadata> = null;
   export const style: Style = {};
 
   export let callback: never;
-
-
+  
   let typedCallback = callback as SuspendedTextOutputCallback | TextOutputCallback;
   let dirtyMessage = typedCallback.getMessage();
   let cleanMessage = sanitize(dirtyMessage);
+  let shouldRenderTextOutputNext = false; 
 
   $: {
     typedCallback = callback as SuspendedTextOutputCallback | TextOutputCallback;
     dirtyMessage = typedCallback.getMessage();
     cleanMessage = sanitize(dirtyMessage);
+    shouldRenderTextOutputNext = stepMetadata !== null && (stepMetadata as StepMetadata).isStepSelfSubmittable && !(stepMetadata as StepMetadata).isUserInputOptional
   }
 </script>
 
 <Text classes="tw_font-bold tw_mt-6">
   {@html cleanMessage}
 </Text>
+
+{#if shouldRenderTextOutputNext}
+    <Button busy={false} style="primary" type="submit" width="full">
+      <T key="nextButton" />
+    </Button>
+{/if}
+

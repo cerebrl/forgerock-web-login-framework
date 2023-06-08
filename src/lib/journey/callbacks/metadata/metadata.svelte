762d8f9d273608353b5dfeee3e1662de55b11504
ryanbas21 <ryan.basmajian@forgerock.com>
2023-06-08 09:21:15 -0600

    feat(callbacks): add-webauthn-support
    add webauthn support for usernameless, passwordless and more to the login widget

---
+++ src/lib/journey/callbacks/metadata/metadata.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import type { z } from 'zod';
+
+  import type {
+    CallbackMetadata,
+    SelfSubmitFunction,
+    StepMetadata,
+  } from '$journey/journey.interfaces';
+  import type { styleSchema } from '$lib/style.store';
+  import type { Maybe } from '$lib/interfaces';
+  import type { MetadataCallback } from '@forgerock/javascript-sdk';
+
+  export let callback: Maybe<MetadataCallback> = null;
+  export let callbackMetadata: Maybe<CallbackMetadata> = null;
+  export const selfSubmitFunction: Maybe<SelfSubmitFunction> = null;
+  export let stepMetadata: Maybe<StepMetadata> = null;
+  export const style: z.infer<typeof styleSchema> = {};
+
+  $: {
+    const data = callback && callback.getData();
+    const payload = callback && callback.payload;
+  }
+</script>

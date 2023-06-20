87bb9e493ec0ed95c1f395eb03859ed37ddd1997
ryanbas21 <ryan.basmajian@forgerock.com>
2023-06-20 12:51:00 -0600

    feat(webauthn): add-webauthn-support
    add webauthn support to the login widget, including recovery code display and using passwordless,
    and usernameles

--- src/lib/journey/callbacks/metadata/metadata.svelte
+++ src/lib/journey/callbacks/metadata/metadata.svelte
@@ -10,14 +10,9 @@
   import type { Maybe } from '$lib/interfaces';
   import type { MetadataCallback } from '@forgerock/javascript-sdk';
 
-  export let callback: Maybe<MetadataCallback> = null;
-  export let callbackMetadata: Maybe<CallbackMetadata> = null;
+  export const callback: Maybe<MetadataCallback> = null;
+  export const callbackMetadata: Maybe<CallbackMetadata> = null;
   export const selfSubmitFunction: Maybe<SelfSubmitFunction> = null;
-  export let stepMetadata: Maybe<StepMetadata> = null;
+  export const stepMetadata: Maybe<StepMetadata> = null;
   export const style: z.infer<typeof styleSchema> = {};
-
-  $: {
-    const data = callback && callback.getData();
-    const payload = callback && callback.payload;
-  }
 </script>
--- src/lib/journey/stages/DeviceAuthentication.svelte
+++
@@ -1,3 +0,0 @@
-<script type="module"></script>
-
-<div />
